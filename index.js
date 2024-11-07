034bf48758a08f815a46dcac761ab30df7b5da78
manishvarma123 <manishvarma1302@gmail.com>
2024-11-07 21:27:14 +0530

    updated index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const router = require('./routes/INDEX.JS')
 const app = express()
 
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : true, // process.env.FRONTEND_URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials : true
 }))
